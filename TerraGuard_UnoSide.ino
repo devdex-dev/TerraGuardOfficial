e37a5d73b21fd4adac4d0a497e10adbd6c7d55b4
dex <106439166+dexxy19@users.noreply.github.com>
2023-10-20 13:03:03 +0800

    Update TerraGuard_UnoSide.ino

--- TerraGuard_UnoSide.ino
+++ TerraGuard_UnoSide.ino
@@ -1,10 +1,20 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 
 // RE and DE Pins set the RS485 module
 // to Receiver or Transmitter mode
 #define RE 8
 #define DE 7
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
 // Modbus RTU requests for reading NPK values
@@ -21,6 +31,17 @@ SoftwareSerial mod(2, 3);
 SoftwareSerial espSerial(1, 0); // RX, TX
 //SoftwareSerial mod(10, 11);
 
+// Variables to store the previous values
+byte prevVal1, prevVal2, prevVal3;
+
+// Number of consecutive stable readings required
+const int stableCount = 5;
+
+// Counter variables to track stability
+int stableVal1Count = 0;
+int stableVal2Count = 0;
+int stableVal3Count = 0;
+
 void setup() {
   // Set the baud rate for the Serial port
   Serial.begin(9600);
@@ -30,15 +51,19 @@ void setup() {
   // Set the baud rate for the SerialSoftware object
   mod.begin(9600);
 
+   // initialize the OLED object
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+   // Clear the buffer.
+  display.clearDisplay();
   // Define pin modes for RE and DE
   pinMode(RE, OUTPUT);
   pinMode(DE, OUTPUT);
-  
-  delay(500);
 }
  
 void loop() {
-   
   // Read values
   byte val1,val2,val3;
   val1 = nitrogen();
@@ -48,22 +73,89 @@ void loop() {
   val3 = potassium();
   delay(500);
 
- // Print values to the serial monitor -OLD
-  Serial.print("n: ");
-  Serial.print(String(val1));
-  Serial.print(" mg/kg");
-  
-  Serial.print(", p: ");
-  Serial.print(String(val2));
-  Serial.print(" mg/kg");
- 
-  Serial.print(", k:");
-  Serial.print(String(val3));
-  Serial.println(" mg/kg");
- 
-  
-   }
+  // Check if the values have stabilized
+  if (val1 == prevVal1) {
+    stableVal1Count++;
+  } else {
+    stableVal1Count = 0;
+    prevVal1 = val1;
+  }
 
+  if (val2 == prevVal2) {
+    stableVal2Count++;
+  } else {
+    stableVal2Count = 0;
+    prevVal2 = val2;
+  }
+
+  if (val3 == prevVal3) {
+    stableVal3Count++;
+  } else {
+    stableVal3Count = 0;
+    prevVal3 = val3;
+  }
+    // delay(5000);
+  // Print values to the serial monitor when stabilized and not all 255
+ if (!(val1 == 255 && val2 == 255 && val3 == 255)) {
+      if (stableVal1Count >= stableCount && stableVal2Count >= stableCount && stableVal3Count >= stableCount){
+  
+      display.setTextColor(WHITE);
+      display.setCursor(0,0);
+      display.setTextSize(1);
+      display.println("Status: Completed!");
+      display.println();
+      display.print("N: ");
+      display.print(val1);
+      display.println(" mg/kg");
+      display.print("P: ");
+      display.print(val2);
+      display.println(" mg/kg");
+      display.print("K: ");
+      display.print(val3);
+      display.print(" mg/kg");
+      display.display();
+      delay(2000);
+      display.clearDisplay();  
+      
+      Serial.print("n: ");
+      Serial.print(val1);
+      Serial.print(" mg/kg, p: ");
+      Serial.print(val2);
+      Serial.print(" mg/kg, k: ");
+      Serial.print(val3);
+      Serial.println(" mg/kg");
+      delay(10000);
+      exit(0); // Terminate the code or loop
+    } else {
+     display.setTextColor(WHITE);
+      display.setCursor(0,0);
+      display.setTextSize(1);
+      display.println("Status: Reading...");
+      display.setCursor(0,24);
+      display.setTextSize(1);
+      display.print("Tip: Press 'RES' to \nread new sample.");
+      display.display();
+     delay(3000);
+      display.clearDisplay();;
+    }
+  } else {
+      display.setTextColor(WHITE);
+      display.setCursor(0,0);
+      display.setTextSize(1);
+      display.println("Status: No Data");
+       display.setTextColor(BLACK, WHITE);
+      display.setCursor(0,24);
+      display.setTextSize(2);
+      display.print("TerraGuard");
+      display.display();
+      // display.print("");
+      display.display();
+      
+      delay(3000);
+      display.clearDisplay();
+     
+  }
+}
  
 byte nitrogen(){
   digitalWrite(DE,HIGH);
